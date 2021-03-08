572444bc9ad3b708bcac518fc566954f69444f95
Maxence <zokiavn51@yadim.dismail.de>
2021-03-08 16:05:51 +0100

    message

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -17,7 +17,7 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#deced0  ",
+  background: "#DEDED0",
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
